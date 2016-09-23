73f758d341868c9d91ee73892d58a1f66e93d971
leonardo <l.castillo1989@Hotmail.com>
2016-09-22 22:44:53 -0400

    woot

--- index.js
+++ index.js
@@ -11,3 +11,41 @@ const episodes = [
 ];
 
 const finaleEpisode = { id: 's06e10', title: 'The Winds of Winter' };
+
+var getNextEpisodeInPlaylist = episodes =>{
+	return episodes[0];
+}
+
+var addToPlaylist = (episodes, finaleEpisode) => {
+  return episodes.concat(finaleEpisode);
+}
+
+var removeFromPlaylist = (episodes,obj) =>{
+  let replacement = episodes.slice(0);
+  let val = replacement.indexOf(obj);
+  replacement.splice(val,1);
+  return replacement;
+}
+
+function bingeWatch(playlist){
+  
+  if(playlist.length === 0){
+  	return "Please let there be more!";
+  }
+   playlist.pop();
+   return bingeWatch(playlist);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
